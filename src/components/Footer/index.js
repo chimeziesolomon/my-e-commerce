8292ca78bf476476bb755e7cbe8e113f76109898
Gregory <gregory11031985@gmail.com>
2021-02-02 00:03:01 +0200

    refactored layout, deleted redundant wrapper components, made sticky footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './styles.scss';
 
 const Footer = (props) => {
-	return (
-		<footer className="footer">
-			<div className="wrap">@ Techintos 2021</div>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <div className="wrap">@ Techintos 2021</div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Recovery/index.js
+++ src/pages/Recovery/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Recovery = (props) => {
-	return (
-		<div>
-			<h1>Email paasword</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Email password</h1>
+    </div>
+  );
 };
 
 export default Recovery;
