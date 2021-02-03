dc2218fddf164e5d79ddd7c428a0f90ef3ae83c5
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2021-02-03 09:03:21 +0100

    current commit

--- src/components/AuthWrapper/index.js
+++ src/components/AuthWrapper/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.scss';
 
 const AuthWrapper = ({ headline, children }) => {
 	return (
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,69 +6,73 @@ import Button from '../forms/Button';
 import { auth } from '../../firebase/utils';
 
 const initialState = {
-  email: '',
-  password: '',
+	email: '',
+	password: ''
 };
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			...initialState
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  }
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    const { email, password } = this.state;
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	}
+	handleSubmit = async (e) => {
+		e.preventDefault();
+		const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({
-        ...initialState,
-      });
-    } catch (err) {
-      //console.log(err);
-    }
-  };
-  render() {
-    const { email, password } = this.state;
-    return (
-      <AuthWrapper>
-        <div className="formWrap">
-          <form onSubmit={this.handleSubmit}>
-            <FormInput
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              handleChange={this.handleChange}
-            />
-            <FormInput
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-              handleChange={this.handleChange}
-            />
-            <Button type="submit">Login</Button>
-            <div className="socialSignin">
-              <div className="row">
-                <Button onClick={alert}>Sign in with Google</Button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </AuthWrapper>
-    );
-  }
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({
+				...initialState
+			});
+		} catch (err) {
+			//console.log(err);
+		}
+	};
+	render() {
+		const { email, password } = this.state;
+
+		const configAuthWrapper = {
+			headline: 'Login'
+		};
+		return (
+			<AuthWrapper {...configAuthWrapper}>
+				<div className="formWrap">
+					<form onSubmit={this.handleSubmit}>
+						<FormInput
+							type="email"
+							name="email"
+							value={email}
+							placeholder="Email"
+							handleChange={this.handleChange}
+						/>
+						<FormInput
+							type="password"
+							name="password"
+							value={password}
+							placeholder="Password"
+							handleChange={this.handleChange}
+						/>
+						<Button type="submit">Login</Button>
+						<div className="socialSignin">
+							<div className="row">
+								<Button onClick={alert}>Sign in with Google</Button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</AuthWrapper>
+		);
+	}
 }
 
 export default Login;
