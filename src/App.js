5d9a5d05c39da1f4a074424516c8ed918cca82d6
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2021-02-05 12:43:42 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,92 +1,85 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './default.scss';
+import { connect } from 'react-redux';
 import Recovery from './pages/Recovery';
 import { auth } from './firebase/utils';
 import Homepage from './pages/Homepage';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Footer from './components/Footer';
-
+import { setCurrentUser } from './redux/User/user.actions';
 import Header from './components/Header';
 
-const initialState = {
-  currentUser: null,
-};
+//const initialState = {
+//	currentUser: null
+//};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-  }
-  authListener = null;
+	//constructor(props) {
+	//	super(props);
+	//	this.state = {
+	//		...initialState
+	//	};
+	//}
+	authListener = null;
 
-  componentDidMount() {
-    this.authListener = auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        this.setState({ currentUser: user });
-        localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({ currentUser: null });
-        localStorage.removeItem('user');
-      }
-      // if (userAuth) {
-      //   const userRef = await handleUserProfile(userAuth);
-      //   userRef.onSnapshot((snapshot) => {
-      //     this.setState({
-      //       currentUser: {
-      //         id: snapshot.id,
-      //         ...snapshot.data(),
-      //       },
-      //     });
-      //   });
-      // }
-      // this.setState({
-      //   ...initialState,
-      // });
-    });
-  }
+	componentDidMount() {
+		const { setCurrentUser } = this.props;
 
-  componentWillUnmount() {
-    this.authListener();
-  }
+		this.authListener = auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				this.setState({ currentUser: user });
+				localStorage.setItem('user', user.uid);
+			} else {
+				this.setState({ currentUser: null });
+				localStorage.removeItem('user');
+			}
+			// if (userAuth) {
+			//   const userRef = await handleUserProfile(userAuth);
+			//   userRef.onSnapshot((snapshot) => {
+			//     this.setState({
+			//       currentUser: {
+			//         id: snapshot.id,
+			//         ...snapshot.data(),
+			//       },
+			//     });
+			//   });
+			// }
+			// this.setState({
+			//   ...initialState,
+			//setCurrentUser(userAuth);
+			// });
+		});
+	}
 
-  render() {
-    const { currentUser } = this.state;
-    return (
-      <Router>
-        <div className="app">
-          <Header currentUser={currentUser} />
-          <Switch>
-            <Route exact path="/" render={() => <Homepage />} />
-            <Route
-              path="/signup"
-              render={() => (currentUser ? <Redirect to="/" /> : <Signup />)}
-            />
-            <Route
-              path="/login"
-              render={() =>
-                currentUser ? (
-                  <Redirect to="/" />
-                ) : (
-                  <Login currentUser={currentUser} />
-                )
-              }
-            />
-            <Route path="/recovery" render={() => <Recovery />} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+	componentWillUnmount() {
+		this.authListener();
+	}
+
+	render() {
+		const { currentUser } = this.props;
+		return (
+			<Router>
+				<div className="app">
+					<Header />
+					<Switch>
+						<Route exact path="/" render={() => <Homepage />} />
+						<Route path="/signup" render={() => (currentUser ? <Redirect to="/" /> : <Signup />)} />
+						<Route path="/login" render={() => (currentUser ? <Redirect to="/" /> : <Login />)} />
+						<Route path="/recovery" render={() => <Recovery />} />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+});
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,47 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 import { auth } from './../../firebase/utils';
 import Logo from './../../assets/logo.jpg';
 
 const Header = (props) => {
-  const { currentUser } = props;
-  return (
-    <header className="header">
-      <div className="wrap">
-        <div className="logo">
-          <Link to="/">
-            <img src={Logo} alt="logo" />
-          </Link>
-        </div>
+	const { currentUser } = props;
+	return (
+		<header className="header">
+			<div className="wrap">
+				<div className="logo">
+					<Link to="/">
+						<img src={Logo} alt="logo" />
+					</Link>
+				</div>
 
-        <div className="callToActions">
-          {currentUser && (
-            <ul>
-              <li>
-                <span onClick={() => auth.signOut()}>LogOut</span>
-              </li>
-            </ul>
-          )}
+				<div className="callToActions">
+					{currentUser && (
+						<ul>
+							<li>
+								<span onClick={() => auth.signOut()}>LogOut</span>
+							</li>
+						</ul>
+					)}
 
-          {!currentUser && (
-            <ul>
-              <li>
-                <Link to="/signup">Signup</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          )}
-        </div>
-      </div>
-    </header>
-  );
+					{!currentUser && (
+						<ul>
+							<li>
+								<Link to="/signup">Signup</Link>
+							</li>
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+						</ul>
+					)}
+				</div>
+			</div>
+		</header>
+	);
 };
 
 Header.defaultProps = {
-  currentUser: null,
+	currentUser: null
 };
 
-export default Header;
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+});
+
+export default connect(mapStateToProps, null)(Header);
